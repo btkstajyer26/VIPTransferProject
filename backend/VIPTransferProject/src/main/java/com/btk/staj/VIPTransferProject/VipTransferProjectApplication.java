cdc71cb2144ab5b3705c78e77f887062c5e2e255
btkstajyer26 <btkstajyer26@gmail.com>
2026-07-01 11:24:23 +0300

    init

---
+++ backend/VIPTransferProject/src/main/java/com/btk/staj/VIPTransferProject/VipTransferProjectApplication.java
@@ -0,0 +1,13 @@
+package com.btk.staj.VIPTransferProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipTransferProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipTransferProjectApplication.class, args);
+	}
+
+}
