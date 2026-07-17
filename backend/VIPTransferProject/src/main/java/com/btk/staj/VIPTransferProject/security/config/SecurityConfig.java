4798c86fe5169aae81922ef06dda97c6184c1530
CeyhunSedef <23181616017@gazi.edu.tr>
2026-07-17 11:19:14 +0300

    feat(localization): Add Localization Service, Translation DTOs, and configure public access

--- backend/VIPTransferProject/src/main/java/com/btk/staj/VIPTransferProject/security/config/SecurityConfig.java
+++ backend/VIPTransferProject/src/main/java/com/btk/staj/VIPTransferProject/security/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/reservations").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/reservations/guest/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/translations/**").permitAll()
                         // Monitoring satırı için bunu ekliyom.
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
