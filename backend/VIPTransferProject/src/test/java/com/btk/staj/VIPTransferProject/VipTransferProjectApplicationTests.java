d6adbc6133d0eaec77c92a460b3f62b618c91d70
CeyhunSedef <23181616017@gazi.edu.tr>
2026-07-22 11:50:47 +0300

    Fix test package declaration

--- backend/VIPTransferProject/src/test/java/com/btk/staj/VIPTransferProject/VipTransferProjectApplicationTests.java
+++ backend/VIPTransferProject/src/test/java/com/btk/staj/VIPTransferProject/VipTransferProjectApplicationTests.java
@@ -1,3 +1,5 @@
+package com.btk.staj.VIPTransferProject;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
